415e50b641ee8c24c9b1e1054dfefeff01d4f77f
ahmadiqball <77543847+ahmadiqball@users.noreply.github.com>
2021-02-09 02:53:49 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function myFunction() {
+  var x = innerWidth;
+  console.log(x);
+  if (x <= 568){
+    $("#backgroundChange").attr("src", "images/Background2.jpg");
+    $("#textChange").text("Hi, ");
+    $("#textChange2").text("I'am Iqbal");
+  }
+  else if (x > 568 && x <= 800) {
+    $("#backgroundChange").attr("src", "images/Background4.jpg");
+    $("#textChange").text("Hi, I'am Iqbal");
+    $("#textChange2").text("");
+  }
+  else {
+    $("#backgroundChange").attr("src", "images/Background.jpg");
+    $("#textChange").text("Hi, I'am Iqbal");
+    $("#textChange2").text("");
+  }
+}
+
+myFunction();// Call listener function at run time
+$(window).resize(function(){
+  myFunction();
+});// Attach listener function on state changes
